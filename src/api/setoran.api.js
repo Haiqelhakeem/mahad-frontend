f0bc1e86bdb2b005d2744c03247d6ae02b7b2933
habbaza <habbazahubbal@gmail.com>
2024-12-01 15:48:53 +0700

    change url api

--- src/api/setoran.api.js
+++ src/api/setoran.api.js
@@ -1,3 +1,3 @@
-export const santriAPI = 'http://mahad-api.vercel.app/api/santri'
-export const mentorAPI = 'http://mahad-api.vercel.app/api/mentors'
+export const santriAPI = 'http://localhost:5000/api/santri'
+export const mentorAPI = 'http://localhost:5000/api/mentors'
 export const setoranAPI = 'http://localhost:5000/api/setoran'
